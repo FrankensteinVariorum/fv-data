2f2d2b84baafa28f3d04685e7157c518c43007b9
Elisa Beshero-Bondar <ebbondar@gmail.com>
2018-10-21 18:53:59 -0400

    completing the levenshtein weighting

--- edit-distance/LevenCalc_toXML.py
+++ edit-distance/LevenCalc_toXML.py
@@ -42,38 +42,93 @@ for line in f: # iterate over the remaining lines
     app = v[0]
     rg1_2 = v[1] + '::' + v[3]
     if '#fMS' in rg1_2:
-        transpose_costs = np.full((128, 128), .5, dtype=np.float64)
+        print('#fMS is here')
+        transpose_costs = np.full((128, 128), 0.25, dtype=np.float64)
         dist1_2 = dam_lev(v[2], v[4])
     else:
         transpose_costs = np.ones((128, 128), dtype=np.float64)
         dist1_2 = dam_lev(v[2], v[4])
 
     rg1_3 = v[1] + '::' + v[5]
-    dist1_3 = dam_lev(v[2], v[6])
+    if '#fMS' in rg1_3:
+        print('#fMS is here')
+        transpose_costs = np.full((128, 128), 0.25, dtype=np.float64)
+        dist1_3 = dam_lev(v[2], v[6])
+    else:
+        transpose_costs = np.ones((128, 128), dtype=np.float64)
+        dist1_3 = dam_lev(v[2], v[6])
 
     rg1_4 = v[1] + '::' + v[7]
-    dist1_4 = dam_lev(v[2], v[8])
+    if '#fMS' in rg1_4:
+        print('#fMS is here')
+        transpose_costs = np.full((128, 128), 0.25, dtype=np.float64)
+        dist1_4 = dam_lev(v[2], v[8])
+    else:
+        transpose_costs = np.ones((128, 128), dtype=np.float64)
+        dist1_4 = dam_lev(v[2], v[8])
 
     rg1_5 = v[1] + '::' + v[9]
-    dist1_5 = dam_lev(v[2], v[10])
+    if '#fMS' in rg1_5:
+        print('#fMS is here')
+        transpose_costs = np.full((128, 128), 0.25, dtype=np.float64)
+        dist1_5 = dam_lev(v[2], v[10])
+    else:
+        transpose_costs = np.ones((128, 128), dtype=np.float64)
+        dist1_5 = dam_lev(v[2], v[10])
 
     rg2_3 = v[3] + '::' + v[5]
-    dist2_3 = dam_lev(v[4], v[6])
+    if '#fMS' in rg2_3:
+        print('#fMS is here')
+        transpose_costs = np.full((128, 128), 0.25, dtype=np.float64)
+        dist2_3 = dam_lev(v[4], v[6])
+    else:
+        transpose_costs = np.ones((128, 128), dtype=np.float64)
+        dist2_3 = dam_lev(v[4], v[6])
 
     rg2_4 = v[3] + '::' + v[7]
-    dist2_4 = dam_lev(v[4], v[8])
+    if '#fMS' in rg2_4:
+        print('#fMS is here')
+        transpose_costs = np.full((128, 128), 0.25, dtype=np.float64)
+        dist2_4 = dam_lev(v[4], v[8])
+    else:
+        transpose_costs = np.ones((128, 128), dtype=np.float64)
+        dist2_4 = dam_lev(v[4], v[8])
 
     rg2_5 = v[3] + '::' + v[9]
-    dist2_5 = dam_lev(v[4], v[10])
+    if '#fMS' in rg2_5:
+        print('#fMS is here')
+        transpose_costs = np.full((128, 128), 0.25, dtype=np.float64)
+        dist2_5 = dam_lev(v[4], v[10])
+    else:
+        transpose_costs = np.ones((128, 128), dtype=np.float64)
+        dist2_5 = dam_lev(v[4], v[10])
 
     rg3_4 = v[5] + '::' + v[7]
-    dist3_4 = dam_lev(v[6], v[8])
+    if '#fMS' in rg3_4:
+        print('#fMS is here')
+        transpose_costs = np.full((128, 128), 0.25, dtype=np.float64)
+        dist3_4 = dam_lev(v[6], v[8])
+    else:
+        transpose_costs = np.ones((128, 128), dtype=np.float64)
+        dist3_4 = dam_lev(v[6], v[8])
 
     rg3_5 = v[5] + '::' + v[9]
-    dist3_5 = dam_lev(v[6], v[10])
+    if '#fMS' in rg3_5:
+        print('#fMS is here')
+        transpose_costs = np.full((128, 128), 0.25, dtype=np.float64)
+        dist3_5 = dam_lev(v[6], v[10])
+    else:
+        transpose_costs = np.ones((128, 128), dtype=np.float64)
+        dist3_5 = dam_lev(v[6], v[10])
 
     rg4_5 = v[7] + '::' + v[9]
-    dist4_5 = dam_lev(v[8], v[10])
+    if '#fMS' in rg4_5:
+        print('#fMS is here')
+        transpose_costs = np.full((128, 128), 0.25, dtype=np.float64)
+        dist4_5 = dam_lev(v[8], v[10])
+    else:
+        transpose_costs = np.ones((128, 128), dtype=np.float64)
+        dist4_5 = dam_lev(v[8], v[10])
 
     FS = ET.SubElement(Root, 'fs', attrib={'feats': app})
     f1_2 = ET.SubElement(FS, 'f', attrib={'name': rg1_2, 'fVal': str(dist1_2)})
@@ -93,27 +148,3 @@ for line in f: # iterate over the remaining lines
 tree = ET.ElementTree(Root)
 tree.write('FV_LevDists-weighted.xml')
 f.close()
-
-    #TRl = E.tr
-    #TD = E.td
-    #the_loop = TRl(
-    #TD(dist1_2),
-    #TD(dist1_3),
-    #TD(dist1_4),
-    #TD(dist2_3),
-    #TD(dist2_4),
-    #TD(dist3_4)
-    #)
-
-#the_doc = Root(
- #       	Table(
-#        TR(TH('Locus'), TH('MS to 1828'), TH('MS to 1837'), TH('MS to 1854'), TH('1828 to 1837'), TH('1828 to 1854'), TH('1837 to 1854'))
-
-    #	the_loop
-        #ebb: having trouble making a nested loop in constructed XML output. Running this produces a fatal error at this line.
-        #If I position the_doc and this tree builder to surround the for loop, this errors on the "for line in f:"
-        #Does the for-loop and its dependents belong in a function?
-        
-     #   )
-           
-       # )  	
---
+++ edit-distance/levenshteinVals.sch
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<sch:schema xmlns:sch="http://purl.oclc.org/dsdl/schematron" queryBinding="xslt2"
+    xmlns:sqf="http://www.schematron-quickfix.com/validator/process">
+    <!--2018-10-21 ebb: This Schematron sends informational reports to evaluate whether an attempt to produce weighted Levenshtein values succeeded. We compare one file without weights against another in which we should likely have produced some weighted values where the fMS witness was present in a rdgGrp. 
+    This schema file should be associated with the FV_LevDists-weighted.xml file.-->
+    <sch:pattern>
+        <sch:let name="unweighted" value="doc('FV_LevDists.xml')"/>
+ <!--       <sch:let name="weighted" value="doc('FV_LevDists-weighted.xml')"/>-->
+   <sch:rule context="f[contains(@name, '#fMS') and not(contains(@name, 'NoRG'))]">
+       <sch:assert test="$unweighted//f[@name = current()/@name]/@fVal ne current()/@fVal" role="information">Here is case where the unweighted version of the levenshtein measurement (<sch:value-of select="$unweighted//f[@name = current()/@name]/@fVal"/>) equals this weighted version.</sch:assert>
+   </sch:rule>     
+    </sch:pattern>
+</sch:schema>
